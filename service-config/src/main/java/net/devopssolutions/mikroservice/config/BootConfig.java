77f0fea68c357492bf45db7043f16cced8e11d2e
Kamil Kiewisz <kamkie@outlook.com>
2014-12-29 18:50:09 +0100

    change configuration of config server

--- service-config/src/main/java/net/devopssolutions/mikroservice/config/BootConfig.java
+++ service-config/src/main/java/net/devopssolutions/mikroservice/config/BootConfig.java
@@ -7,14 +7,11 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
 @EnableConfigServer
-@PropertySources(value = {@PropertySource("classpath:configServer.properties")})
 public class BootConfig extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
