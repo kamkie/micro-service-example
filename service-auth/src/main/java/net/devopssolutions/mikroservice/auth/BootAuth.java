3cb1cafef82355ee867233528a6f56180a59107f
Kamil Kiewisz <kamkie@outlook.com>
2014-12-29 20:03:13 +0100

    start working on auth service

--- service-auth/src/main/java/net/devopssolutions/mikroservice/auth/BootAuth.java
+++ service-auth/src/main/java/net/devopssolutions/mikroservice/auth/BootAuth.java
@@ -1,27 +1,25 @@
-package net.devopssolutions.mikroservice.config;
+package net.devopssolutions.mikroservice.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
-@EnableConfigServer
 @SpringApplicationConfiguration
-public class BootConfig extends SpringBootServletInitializer {
+public class BootAuth extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootConfig.class, args);
+        SpringApplication.run(BootAuth.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BootConfig.class);
+        return application.sources(BootAuth.class);
     }
 }
