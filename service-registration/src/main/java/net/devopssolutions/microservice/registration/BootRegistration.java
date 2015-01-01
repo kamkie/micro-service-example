fb5e8d60025ee9abf12b3d1e95550beefcbba5c9
Kamil Kiewisz <kamkie@outlook.com>
2015-01-01 02:01:19 +0100

    add eureka

---
+++ service-registration/src/main/java/net/devopssolutions/microservice/registration/BootRegistration.java
@@ -0,0 +1,29 @@
+package net.devopssolutions.microservice.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
+@EnableEurekaServer
+@EnableDiscoveryClient
+@SpringApplicationConfiguration
+public class BootRegistration extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRegistration.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BootRegistration.class);
+    }
+}
