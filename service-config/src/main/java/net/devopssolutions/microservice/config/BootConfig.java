97a476bee1ec6e53f28ccc1b4192b79cd8fe8a83
Kamil Kiewisz <kamkie@outlook.com>
2015-01-01 02:19:40 +0100

    service registration with eureka

--- service-config/src/main/java/net/devopssolutions/microservice/config/BootConfig.java
+++ service-config/src/main/java/net/devopssolutions/microservice/config/BootConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringApplicationConfiguration
 public class BootConfig extends SpringBootServletInitializer {
 
