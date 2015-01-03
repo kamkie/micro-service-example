0bac3ac284038b1a5903e2ec392fd5893d73eb39
Kamil Kiewisz <kamkie@outlook.com>
2015-01-03 23:51:55 +0100

    enable caching

--- service-auth/src/main/java/net/devopssolutions/microservice/auth/BootAuth.java
+++ service-auth/src/main/java/net/devopssolutions/microservice/auth/BootAuth.java
@@ -6,6 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +19,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan
 @EnableJpaRepositories
 @EnableDiscoveryClient
+@EnableCaching
 public class BootAuth extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
