46d491057dc689f169ea7ac7e744884bc423a81d
Kamil Kiewisz <kamkie@outlook.com>
2015-01-04 04:40:26 +0100

    init service integration with camel

---
+++ service-integration/src/main/java/net/devopssolutions/microservice/integration/BootIntegration.java
@@ -0,0 +1,40 @@
+package net.devopssolutions.microservice.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.FeignClientScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan(basePackages = "net.devopssolutions.microservice")
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@FeignClientScan
+@EnableCaching
+public class BootIntegration extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootIntegration.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BootIntegration.class);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager("passwordEncoder");
+        return cacheManager;
+    }
+}
