212666468e5b367c80a0d87572ed1636eea896fe
Kamil Kiewisz <kamkie@outlook.com>
2015-01-06 02:36:32 +0100

    remove imports

--- service-integration/src/main/java/net/devopssolutions/microservice/integration/controller/HomeController.java
+++ service-integration/src/main/java/net/devopssolutions/microservice/integration/controller/HomeController.java
@@ -1,12 +1,10 @@
 package net.devopssolutions.microservice.integration.controller;
 
-import net.devopssolutions.microservice.integration.routes.RouteRegistry;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Route;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
