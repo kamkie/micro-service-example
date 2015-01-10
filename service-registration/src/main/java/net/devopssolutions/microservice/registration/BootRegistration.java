7bd794b72a0d9b3191497a670f547eab5469d6b2
Kamil Kiewisz <kamkie@outlook.com>
2015-01-10 18:38:27 +0100

    changes in eureka configuration

--- service-registration/src/main/java/net/devopssolutions/microservice/registration/BootRegistration.java
+++ service-registration/src/main/java/net/devopssolutions/microservice/registration/BootRegistration.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.FeignClientScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +20,9 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "net.devopssolutions.microservice")
 @EnableEurekaServer
 @EnableDiscoveryClient
-@SpringApplicationConfiguration
+@EnableCircuitBreaker
+@FeignClientScan
+@EnableCaching
 public class BootRegistration extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
@@ -28,5 +32,11 @@ public class BootRegistration extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(BootRegistration.class);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager("passwordEncoder");
+        return cacheManager;
     }
 }
---
+++ service-registration/src/main/java/net/devopssolutions/microservice/registration/security/WebSecurityConfig.java
@@ -0,0 +1,67 @@
+package net.devopssolutions.microservice.registration.security;
+
+import net.devopssolutions.microservice.security.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(customAuthenticationProvider);
+    }
+
+    @Configuration
+    @Order(2)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/**");
+
+            http.csrf().disable();
+
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+            http
+                    .authorizeRequests()
+                    .anyRequest().hasRole("USER")
+                    .and()
+                    .httpBasic();
+        }
+    }
+
+    @Configuration
+    @Order(1)
+    public static class AdminWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/admin/**");
+
+            http.csrf().disable();
+
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+            http.anonymous();
+//            http
+//                    .authorizeRequests()
+//                    .anyRequest().hasRole("USER")
+//                    .and()
+//                    .httpBasic();
+        }
+    }
+
+}
